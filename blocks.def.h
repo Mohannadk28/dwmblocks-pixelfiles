24945a9fc26f14a0290dfd117d8c6980ac02c772
Mohannadk28 <73769579+Mohannadk28@users.noreply.github.com>
2023-02-01 12:35:23 +0300

    UPDATE

--- blocks.def.h
+++ blocks.def.h
@@ -1,9 +1,15 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{"Mem:", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	30,		0},
-
-	{"Clock", "date  '+%I:%M %p '",					5,		0},
+  { "󰖟  ", "bash ~/.dwm/blocks/lang.sh",          1,    0 },
+  { "  ", "bash ~/.dwm/blocks/kernal.sh",        0,    0 },
+	{ "󰍛  ", "bash ~/.dwm/blocks/mem.sh",	          5,		0 },
+  { "󰻠  ", "bash ~/.dwm/blocks/cpu.sh",           5,    0 },
+  { "󰃞  ", "bash ~/.dwm/blocks/bright.sh",        1,    0 },
+  { "  ", "bash ~/.dwm/blocks/bat.sh",           5,    0 },
+  { "󰕾  ", "bash ~/.dwm/blocks/vol.sh",           1,    0 },
+  { "  ", "bash ~/.dwm/blocks/cal.sh",           8,    0 },
+	{ "  ", "bash ~/.dwm/blocks/time.sh",					1,		0 },
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
